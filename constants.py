2152a75f4b6d25b210c97dd7b981f56c00e61261
cmtrapp02 <cmtrapp02@gmail.com>
2018-06-19 16:07:23 -0400

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,16 @@
+#game constants
+
+class colors:
+
+    BLACK = (0,0,0)
+    WHITE = (255,255,255)
+    RED = (255,0,0)
+    GREEN = (0,255,0)
+    BLUE = (0,0,255)
+    ORANGE = (255,165,0)
+
+
+class game:
+
+    GRAVITY = 9.8
+    SCREENRECT = Rect(0, 0, 1024, 786)
