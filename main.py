72e23297c47a53b6705568e61cef1496761bfce7
cmtrapp02 <cmtrapp02@gmail.com>
2018-08-10 01:32:30 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, img):
         pygame.sprite.Sprite.__init__(self)
         
-        self.speed = 5
+        self.speed = 3
         self.vel = ([5,5])
         self.i = img
         self.image = pygame.image.load(self.i)
@@ -21,16 +21,33 @@ class Player(pygame.sprite.Sprite):
         self.facing = 1
 
     #move the player
-    def move(self, direction, pos_y):
-        
-        self.facing = direction
-        self.rect.move_ip(direction * self.speed, pos_y * self.speed)
+    def move(self):
+        self.direction = 0
+        self.facing = self.direction
+        pressed = pygame.key.get_pressed()
+        self.pos_y = 0
+        if pressed[K_a]:
+            self.direction += -1
+        if pressed[K_d]:
+            self.direction += 1
+        if pressed[K_w]:
+            self.pos_y += -1
+        if pressed[K_s]:
+            self.pos_y += 1
+        self.rect.move_ip(self.direction * self.speed, self.pos_y * self.speed)
         #self.rect.top = 100
         
-        
+    def collide(self, blocking_group, offset):
+
+        mask = pygame.sprite.collide_mask
+        self.collide = pygame.sprite.spritecollide(self, blocking_group, False, mask)
+        if self.direction - offset == 0:
+            self.direction = 0
+
 class Rects(pygame.sprite.Sprite):
 
     def __init__(self, img):
+        
         pygame.sprite.Sprite.__init__(self)
         self.test = ([1,1])
         self.i = img
@@ -40,117 +57,9 @@ class Rects(pygame.sprite.Sprite):
         
     
     def update(self, x, y):
-
-        self.rect.x = x
-        self.rect.y = y
-
-
-def vadd(x, y):
-    return [x[0]+y[0],x[1]+y[1]]
-
-def vsub(x, y):
-    return [x[0]-y[0],x[1]-y[1]]
-
-def vdot(x, y):
-    return [x[0]*y[0]+x[1]*y[1]]
-
-
-def collision_normal(left_mask, right_mask, left_pos, right_pos):
-
-    offset = list(map(int, vsub(left_pos, right_pos)))
-
-    overlap = left_mask.mask.overlap_area(right_mask.mask, offset)
-
-    if overlap == 0:
-        return
-    
-    """Calculate collision normal"""
-    
-    nx = (left_mask.mask.overlap_area(right_mask.mask,(offset[0]+1,offset[1])) -
-          left_mask.mask.overlap_area(right_mask.mask,(offset[0]-1,offset[1])))
-    ny = (left_mask.mask.overlap_area(right_mask.mask,(offset[0],offset[1]+1)) -
-          left_mask.mask.overlap_area(right_mask.mask,(offset[0],offset[1]-1)))
-    if nx == 0 and ny == 0:
-        print('overlap')
-        return None, overlap
-
-    n = [nx, ny]
-
-    overlap2 = left_mask.mask.overlap_area(right_mask.mask, (n))
-    if overlap2:
-        print(nx)
-
-    return n
-
-def detect_collision(player, group, sprites):
-
-    
-    offset = [0, 0, 0, 0]
-    mask = pygame.sprite.collide_mask
-    if pygame.sprite.spritecollide(player, group, False, mask):
-
-        for index in range(0, len(sprites)):
-                
-            offset_x = player.rect.right - sprites[index].rect.right
-            offset_y = player.rect.bottom - sprites[index].rect.bottom
-
-            overlap_area = player.mask.overlap_area(sprites[index].mask, (offset_x, offset_y))
-        
-            index += 1
-            RIGHT, LEFT, BOTTOM, TOP = 0, 0, 0, 0
-            if overlap_area:
-
-                print(player.rect)
-
-                if offset_x <= -1:
-                    print("Right: " + str(offset_x))
-                    RIGHT = Playercollision.RIGHT
-                if offset_x >= 1:
-                    print("Left: " + str(offset_x))
-                    LEFT = Playercollision.LEFT
-                if offset_y <= -1:
-                    print("Bottom: " + str(offset_y))
-                    BOTTOM = Playercollision.BOTTOM
-                if offset_y >= 1:
-                    print("Top: " + str(offset_y))
-                    TOP = Playercollision.TOP
-
-                offset = [RIGHT, LEFT, BOTTOM, TOP]
-
-    return offset
-            
-                
-def player_input(player, collision):
-
-    pressed = pygame.key.get_pressed()
-    
-    direction = 0
-    pos_y = 0
-    if pressed[K_a]:
-        direction += -1
-        if collision[1] == Playercollision.LEFT:
-            direction = 0
-    if pressed[K_d]:
-        direction += 1
-        if collision[0] == Playercollision.RIGHT:
-            direction = 0
-    if pressed[K_w]:
-        pos_y += -1
-        if collision[3] == Playercollision.TOP:
-            pos_y = 0
-    if pressed[K_s]:
-        pos_y += 1
-        if collision[2] == Playercollision.BOTTOM:
-            pos_y = 0
-
-    player.move(direction, pos_y)
-
-
-def apply_gravity(pos_y):
-
-    #TODO: apply gravity
-    gravity = constants.Game.GRAVITY
-    pos_y += gravity
+        self.x = 0
+        self.y = 0
+        self.rect.move_ip(self.x, self.y)
 
  #main function for calling other functions       
 def main(winstyle = 0):
@@ -183,7 +92,7 @@ def main(winstyle = 0):
     test_ground = Rects(ground_image)
 
     #update the rect locations
-    test_rect.update(100, 225)
+    test_rect.update(50, 225)
     test_rect2.update(400, 225)
     test_rect3.update(200, 100)
     test_background.update(0, 0)
@@ -221,8 +130,8 @@ def main(winstyle = 0):
         sprites = [test_rect, test_rect2, test_rect3]
         
         #handle player inputs and collision
-        collision = detect_collision(player, blocking_group, sprites)
-        player_input(player, collision)
+        player.move()
+        player.collide(blocking_group, test_rect3.rect.x)
 
         #draw the scene
         dirty = all.draw(surface)
@@ -234,7 +143,7 @@ def main(winstyle = 0):
 
         #TODO: fix timestep
         #Cap the frame rate at 40 TEMPORARY
-        clock.tick(40)
+        clock.tick(60)
         
         
 
